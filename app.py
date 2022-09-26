a50cfe18d654d4e616145fce3528507179424e90
Alex McGarity <amcgarity123@gmail.com>
2022-09-26 14:44:13 -0500

    configured google sign in to be centered

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import flet
 from flet import IconButton, Page, Row, TextField, icons
 
+
 def main(page: Page):
-    page.title = "Flet counter example"
+    page.title = "Subcontractor App"
     page.vertical_alignment = "center"
 
-    txt_number = TextField(value="0", text_align="right", width=100)
-
-    #google sign in
+    # google sign in
     def on_google_sign_in():
         print("Google sign in")
-    google_sign_in = IconButton(icon=icons.google, on_click=on_google_sign_in)
-    page.add(google_sign_in)
-#run in native OS window
+    google_sign_in = IconButton(icon=icons.LOGIN, on_click=on_google_sign_in())
+    page.add(Row([
+        google_sign_in
+    ], alignment="center"))
+
+
+# run in native OS window
 flet.app(target=main)
-#run as web app
+# run as web app
 #flet.app(target=main, view=flet.WEB_BROWSER)
