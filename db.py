5f6c0b492c6ab16a1caad38eceddac6121f66f98
Jonathan Montrose <64240200+jem0ntr053@users.noreply.github.com>
2022-10-03 09:47:27 -0500

    Did some formatting to test commit.

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+
 def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(
@@ -14,11 +15,13 @@ def get_db():
 
     return g.db
 
+
 def close_db(e=None):
     db = g.pop("db", None)
 
     if db is not None:
         db.close()
+
 
 def init_db():
     db = get_db()
@@ -26,12 +29,14 @@ def init_db():
     with current_app.open_resource("schema.sql") as f:
         db.executescript(f.read().decode("utf8"))
 
+
 @click.command("init-db")
 @with_appcontext
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo("Initialized the database.")
+
 
 def init_app(app):
     app.teardown_appcontext(close_db)
