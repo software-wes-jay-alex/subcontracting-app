1d2a082345867273be0fa70029859d43bde0565b
VoteYesForWes <104099852+VoteYesForWes@users.noreply.github.com>
2022-10-13 18:16:01 -0500

    made a database for the list function in the app

--- schema.sql
+++ schema.sql
@@ -1,6 +1,12 @@
-CREATE TABLE user (
-  id TEXT PRIMARY KEY,
-  name TEXT NOT NULL,
-  email TEXT UNIQUE NOT NULL,
-  profile_pic TEXT NOT NULL
-);
+CREATE TABLE `items` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `qty` int(11) NOT NULL,
+  `got` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `items`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
