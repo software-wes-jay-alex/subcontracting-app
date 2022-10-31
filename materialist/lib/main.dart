6fa76e5d2831e5d7b0232e7baefeb90e351488e6
Alex McGarity <amcgarity123@gmail.com>
2022-10-31 16:44:50 -0500

    added web functionality, tested with chrome

--- materialist/lib/main.dart
+++ materialist/lib/main.dart
@@ -1,3 +1,5 @@
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'bloc/bloc_exports.dart';
@@ -9,7 +11,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final storage = await HydratedStorage.build(
-      storageDirectory: await getApplicationDocumentsDirectory());
+    storageDirectory: kIsWeb
+      ? HydratedStorage.webStorageDirectory
+      : await getApplicationDocumentsDirectory(),
+  );
   HydratedBlocOverrides.runZoned(
     () => runApp(MyApp(
       appRouter: AppRouter(),
