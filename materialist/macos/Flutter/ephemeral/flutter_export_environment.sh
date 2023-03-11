4ebf2372a2d14fa309b06ddf91145197ab9b4e7a
Jonathan Montrose <64240200+jem0ntr053@users.noreply.github.com>
2023-03-11 14:54:35 -0600

    Commit after cancellation of flutterflow

--- materialist/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ materialist/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\amcga\projects\subcontracting-app\materialist"
+export "FLUTTER_ROOT=/Users/montrose/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/montrose/Programming/GitHub/subcontracting-app/materialist"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
